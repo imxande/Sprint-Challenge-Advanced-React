085cc4de9c5ddb6fd433c3421f84d8320fac6d31
imxande <alerodriguezlarosa@gmail.com>
2019-12-07 15:49:25 -0500

    keeping track of my state with onchange and preventing form default behavior with onSubmit

--- client/src/SearchBar.js
+++ client/src/SearchBar.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function SearchBar(){
+    const [pname, setPname] = useState('')
     return(
+        
         <div>
-            <form>
+            <h3>The name is {pname || 'unknown'}   </h3>
+
+            <form 
+                onSubmit = {e =>{
+                    e.preventDefault();
+                }}
+            >
                 <label htmlFor = 'pname'>Search by name: </label>
                 <input
-                maxLength = '15' 
+                maxLength = '50' 
                 id = 'pname'
                 name = 'name'
                 type = 'text'
                 placeholder = " Enter player's name..."
+                onChange = {event => {
+                    setPname(event.target.value)
+                }}
+
                 >
                 </input>
             </form>
